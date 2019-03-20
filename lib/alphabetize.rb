280a3313aac86c2b323f22cc986056d95e37df43
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-20 17:24:08 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,23 @@
 def alphabetize(arr)
   # code here
+  esperanton = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  new_arr = []
+  arr.sort!
+  esperanton.each do |alphabet|
+    arr.each do |string|
+      if alphabet == string[0]
+        new_arr << string
+      end
+    end
+  end
+  new_arr
 end
+
+# ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+# def alphabetize(arr)
+#   new_arr = arr.sort_by do |word|
+#     word.split('').map do |letter|
+#       ALPHABET.index(letter)
+#     end
+#   end
+# end
